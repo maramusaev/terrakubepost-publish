461b05f55e6c4b93ce6f2bf643eacfcb0915c10d
maramusaev <135449136+maramusaev@users.noreply.github.com>
2023-08-05 14:07:18 -0500

    Update variables.tf

--- ekskube/cluster/variables.tf
+++ ekskube/cluster/variables.tf
@@ -1,13 +1,13 @@
 variable "cluster_name"{
-    default = "<name>"
+    default = "cluster_name"
 }
 
 variable "state_bucket"{
-    default = "<name>"
+    default = "maramusaev-state-b"
 }
 
 variable "state_key"{
-    default = "<key_val>"
+    default = "key_val"
 }
 
 variable "state_region"{
